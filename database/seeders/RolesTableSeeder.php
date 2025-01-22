49170a17504f4bc48b105c3e49b190b1019803b0
pramana yuda <pramanayuda772@gmail.com>
2025-01-22 19:40:50 +0700

    push

--- database/seeders/RolesTableSeeder.php
+++ database/seeders/RolesTableSeeder.php
@@ -16,19 +16,19 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
-        // $super_administrator = Role::create(['name' => 'super_administrator']);
-        // $administrator = Role::create(['name' => 'administrator']);
-        // $staff = Role::create(['name' => 'kontributor_utama']);
-        // $staff = Role::create(['name' => 'kontributor_daerah']);
+        $super_administrator = Role::create(['name' => 'super_administrator']);
+        $administrator = Role::create(['name' => 'administrator']);
+        $staff = Role::create(['name' => 'kontributor_utama']);
+        $staff = Role::create(['name' => 'kontributor_daerah']);
 
 
-        // // Super Admin
-        // $user = User::where('username', '199407292022031002')->first();
-        // $user->assignRole('super_administrator');
+        // Super Admin
+        $user = User::where('username', '199407292022031002')->first();
+        $user->assignRole('super_administrator');
 
         // // Administrator
-        // $user = User::where('username', '198605082011011013')->first();
-        // $user->assignRole('administrator');
+        $user = User::where('username', '197610182009011004')->first();
+        $user->assignRole('administrator');
 
         // // Staff Administrator
         // $user = User::where('username', '197807302007011009')->first();
@@ -54,56 +54,56 @@ class RolesTableSeeder extends Seeder
         // $user = \App\Models\User::where('username', 'adm.pesisirselatan')->first();
         // $user->assignRole('kontributor_daerah');
         #3
-        $user = \App\Models\User::where('username', 'adm.kabsolok')->first();
-        $user->assignRole('kontributor_daerah');
-        #4
-        $user = \App\Models\User::where('username', 'adm.sijunjung')->first();
-        $user->assignRole('kontributor_daerah');
-        #5
-        $user = \App\Models\User::where('username', 'adm.tanahdatar')->first();
-        $user->assignRole('kontributor_daerah');
-        #6
-        $user = \App\Models\User::where('username', 'adm.padangpariaman')->first();
-        $user->assignRole('kontributor_daerah');
-        #7
-        $user = \App\Models\User::where('username', 'adm.agam')->first();
-        $user->assignRole('kontributor_daerah');
-        #8
-        $user = \App\Models\User::where('username', 'adm.limapuluhkota')->first();
-        $user->assignRole('kontributor_daerah');
-        #9
-        $user = \App\Models\User::where('username', 'adm.pasaman')->first();
-        $user->assignRole('kontributor_daerah');
-        #10
-        $user = \App\Models\User::where('username', 'adm.solokselatan')->first();
-        $user->assignRole('kontributor_daerah');
-        #11
-        $user = \App\Models\User::where('username', 'adm.dharmasraya')->first();
-        $user->assignRole('kontributor_daerah');
-        #12
-        $user = \App\Models\User::where('username', 'adm.pasamanbarat')->first();
-        $user->assignRole('kontributor_daerah');
-        #13
-        $user = \App\Models\User::where('username', 'adm.padang')->first();
-        $user->assignRole('kontributor_daerah');
-        #14
-        $user = \App\Models\User::where('username', 'adm.kotasolok')->first();
-        $user->assignRole('kontributor_daerah');
-        #15
-        $user = \App\Models\User::where('username', 'adm.sawahlunto')->first();
-        $user->assignRole('kontributor_daerah');
-        #16
-        $user = \App\Models\User::where('username', 'adm.padangpanjang')->first();
-        $user->assignRole('kontributor_daerah');
-        #17
-        $user = \App\Models\User::where('username', 'adm.bukittinggi')->first();
-        $user->assignRole('kontributor_daerah');
-        #18
-        $user = \App\Models\User::where('username', 'adm.payakumbuh')->first();
-        $user->assignRole('kontributor_daerah');
-        #19
-        $user = \App\Models\User::where('username', 'adm.pariaman')->first();
-        $user->assignRole('kontributor_daerah');
+        // $user = \App\Models\User::where('username', 'adm.kabsolok')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #4
+        // $user = \App\Models\User::where('username', 'adm.sijunjung')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #5
+        // $user = \App\Models\User::where('username', 'adm.tanahdatar')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #6
+        // $user = \App\Models\User::where('username', 'adm.padangpariaman')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #7
+        // $user = \App\Models\User::where('username', 'adm.agam')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #8
+        // $user = \App\Models\User::where('username', 'adm.limapuluhkota')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #9
+        // $user = \App\Models\User::where('username', 'adm.pasaman')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #10
+        // $user = \App\Models\User::where('username', 'adm.solokselatan')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #11
+        // $user = \App\Models\User::where('username', 'adm.dharmasraya')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #12
+        // $user = \App\Models\User::where('username', 'adm.pasamanbarat')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #13
+        // $user = \App\Models\User::where('username', 'adm.padang')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #14
+        // $user = \App\Models\User::where('username', 'adm.kotasolok')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #15
+        // $user = \App\Models\User::where('username', 'adm.sawahlunto')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #16
+        // $user = \App\Models\User::where('username', 'adm.padangpanjang')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #17
+        // $user = \App\Models\User::where('username', 'adm.bukittinggi')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #18
+        // $user = \App\Models\User::where('username', 'adm.payakumbuh')->first();
+        // $user->assignRole('kontributor_daerah');
+        // #19
+        // $user = \App\Models\User::where('username', 'adm.pariaman')->first();
+        // $user->assignRole('kontributor_daerah');
         
 
         // Permissions
